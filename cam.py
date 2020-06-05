1f3cb1e97c1ef14a2f81467cce25deeac05f0918
dyoann <dyoann@hotmail.com>
2020-06-05 12:32:51 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -6,23 +6,28 @@ image_width = 224
 image_height= 224
 
 def generate_cam(model,base_path,output_path,img_name,target_class):
+  #Read image, resize and normalize it
   img = cv2.imread(base_path+img_name)
   img = cv2.resize(img, (image_width,image_height), interpolation = cv2.INTER_LANCZOS4)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
   img = img / 255.
   
+  #Get the input weights to the softmax.
   class_weights = model.layers[-2].get_weights()[0]
   conv_model = keras.Model(inputs=model.input, outputs=model.get_layer('GAP').input)
   conv_outputs = conv_model.predict(np.array([img]))[0]
   
+  #Create the class activation map.
   cam = np.zeros(dtype = np.float32, shape = conv_outputs.shape[0:2])
-  target_class = target_class
   for i, w in enumerate(class_weights[:, target_class]):
     cam += w * conv_outputs[:, :, i]
-        
+
+  #Fix and normalize output
   cam = np.maximum(cam, 0.)
   cam = cam / np.max(cam) # 0 is red in COLORMAP_JET, revert
   cam = np.array(cam * 255, dtype = np.uint8)
+  
+  #Write the resulting image
   heatmap_cam = cv2.applyColorMap(cam, cv2.COLORMAP_JET)/255.
   heatmap_cam = cv2.resize(heatmap_cam, (224,224), interpolation = cv2.INTER_LANCZOS4)
   merged = cv2.addWeighted(heatmap_cam, 0.5, img, 0.5, 0)
